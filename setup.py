7a2a6311d1f8507dbd3095c295c7c2abeaa5d6de
yashvantsaroj <yashwantsarojmau@gmail.com>
2025-11-01 20:15:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='Medical_chatbot',
+    version="0.1.0",
+    author="Yashvant Saroj",
+    author_email="yashwantsarojmau@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
